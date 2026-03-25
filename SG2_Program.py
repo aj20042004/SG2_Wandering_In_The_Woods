2a829e0a09ec33fd7d2a935c93f586ec728cd1e7
afan21 <afan2211@gmail.com>
2026-03-25 02:45:32 -0500

    style: added opening comment block add: added boilerplate

---
+++ SG2_Program.py
@@ -0,0 +1,59 @@
+"""
+╔═══════════════════════════════════════════════════════════════════╗
+║ SG2                                                               ║
+╠━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╣
+║ Language: Python 3                                                ║
+║ IDE: Thonny, VSCode                                               ║
+║ Class: CS 4500                                                    ║
+║ Program: SG2                                                      ║
+║ Group Members:                                                    ║
+║  - AJ Soma Ravichandran [aj20042004]                              ║
+║  - Alex Fahnestock      [afan2211]                                ║
+║  - Jackie Herbstreit    []                                        ║
+║  - May Salahaldin       []                                        ║
+║  - Tressa Millering     []                                        ║
+╟───────────────────────────────────────────────────────────────────╢
+║ Program Description                                               ║
+╠━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╣
+║                                                                    
+╟───────────────────────────────────────────────────────────────────╢
+║ Revision History                                                  ║
+╠━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╣
+║ Creation Date: 03/11/2026                                         ║
+║                                                                   ║
+║ Major Revision:                                                   ║
+║	- 03/24/2026 [Alex Fahnestock]                                  ║
+║                                                                   ║
+╟───────────────────────────────────────────────────────────────────╢
+║ Packages Used                                                     ║
+╠━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╣
+║ 
+║ 
+║
+╚═══════════════════════════════════════════════════════════════════╝
+"""
+
+# Imports from numpy go here 
+import numpy as np
+
+# Imports from matplotlib go here 
+import matplotlib.pyplot as plt 
+
+# Type hint related imports go here 
+import numpy.typing		 as npt
+import matplotlib.typing as mpt
+from typing import NamedTuple,final,Final,List,Any,Type
+
+from copy import deepcopy
+
+
+
+
+
+
+
+def main()->None:
+	pass #[afan2211] placeholder no-op. Replace with code
+
+if __name__=="__main__":    
+	main()
