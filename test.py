050cb7548c8771ccaf28f4e784d3de2c0616be74
afan21 <afan2211@gmail.com>
2026-04-09 15:22:44 -0500

    downgraded testing_utilities

--- test.py
+++ test.py
@@ -27,80 +27,52 @@ from collections.abc import Buffer as ReadableBuffer
 
 
 
-def test1():
-	revert_input_patch=testing_utilities.simulate_input(SG2_Program,["a","b"])
-	SG2_Program.main()
-	revert_input_patch()
 
-def test2():
-	reel=[["a","b"],["b","c"],["c","d"]]
-	for tape in reel:
-		revert_input_patch=testing_utilities.simulate_input(SG2_Program,["a","b"])
-		SG2_Program.main()
-		revert_input_patch()
+#def gen_write_patch()->callable[]:
 
 
-def test3():
-	
-	revert=testing_utilities.patch_module(SG2_Program.main,"plt.subplots")
-	SG2_Program.main()
-	revert()
-
-
-
-
-
-
-class dummy_file():
-	def __enter__(self):
-		return self
-	
-	def __exit__(self,exc_type, exc_value, traceback):
-		pass
-
-	def write(self,s: ReadableBuffer,/) -> int:
-		print(s)
-		return 0
-
-
-def dummy_open(*args,**kwargs): 
-	return dummy_file()
-
-
-
-
-
-def test4():
-	""" Test patching instances via hijacking a factory method. Patches a file instances write method."""
-	#possible interface? scope,instance constructor,instance_method_to_patch,patch
-	
-	revert_intercept=testing_utilities.patch_module(SG2_Program.write_results,"Path.open",dummy_open)
-	
-	try:
-		SG2_Program.main()
-	finally:                                                             
-		revert_intercept()
-		
+#pp({ k:v for k,v in vars(SG2_Program).items() if (len(str(v))<80) })
 
 
 #basic sim test
-def test5():
+def test1():
+
+	class dummy_file():
+		def __enter__(self):
+			return self
+		
+		def __exit__(self,exc_type, exc_value, traceback):
+			pass
+
+		def write(self,s: ReadableBuffer,/) -> int:
+			print(s)
+			return 0
+
+
+	def dummy_open(*args,**kwargs): 
+		"""
+		patch for the file factory function Path.open. Allows us to swap out 
+		the file object with our own dummy version.
+		"""
+		return dummy_file()
 	
-	print(SG2_Program.run_simulations(50,1000,300))
+	def dummy_show():
+		return
+	revert_input_patch=testing_utilities.simulate_input(SG2_Program,["10","1000","50",""])
+	revert_noshow_patch=testing_utilities.patch_module(SG2_Program.simulation_analysis_and_histogram,"plt.show",dummy_show)
+	revert_nowrite_patch=testing_utilities.patch_module(SG2_Program.write_results,"Path.open",dummy_open)
+	
+	try:
+		SG2_Program.main()
+	finally:
+		revert_noshow_patch()
+		revert_nowrite_patch()
+		revert_input_patch()
+	
 
 
 def main():
-	print("dummy")
-
-	#testing_utilities.monkey_patch_func(SG2_Program.plt,"subplots",wrapper)
-	#testing_utilities.monkey_patch_func(mp.axes.Axes,"hist",over)
-	#testing_utilities.monkey_patch_func(SG2_Program.main,"ax",new_ax)
-
-	#load(plot,plt)
-	#print(plt.subplots)
-	#setattr(SG2_Program.main,"plt",plt)
-	
-
+	test1()
 
 
 
--- testing_utilities.py
+++ testing_utilities.py
@@ -1,52 +1,159 @@
+
+"""
+====================================================================
+	ABANDON HOPE ALL YE WHO ENTER HERE
+=====================================================================
+	this file is currently filled with all sorts of 
+	garbage due to me having to downgrade patch_module 
+	to a previous iteration. It should serve our purposes 
+	for now 
+======================================================================
+"""
+
 # Authors: Alex Fahnestock 
 # Date: 3/25/26
 # Project: General utility 
 # summary: self explanitory 
 
-from typing import Iterable
+from typing import Iterable,Type,Any,TypedDict
 from collections import deque
 from types import ModuleType
+from collections.abc import Iterator,Callable
+import inspect
+from sys import modules,_getframe
+from dataclasses import dataclass
 
 
-# This is setup weird because we only ever want one.
-class InputAutofeed():
-	"""
-	Testing utility that simulates user input whenever the built in input
-	function is called.
-	"""
-	_feed_tape:deque[str]=deque()
-	_loaded_module:ModuleType|None=None
 
-	@staticmethod
+
+
+
+
+def copy_module_to_class(source:ModuleType,dest:Type):
+
+	for name, obj in inspect.getmembers(source):
+
+		#check that object originates from source 
+		if inspect.getmodule(obj)==inspect.getmodule(source ):
+			setattr(dest, name, obj)
+
+
+
+
+
+@dataclass
+class PatchEntry():
+	id:int
+	apply_to:str
+	label:str
+	restore_callback:Callable[[],None]
+	original_func:Callable
+
+
+
+
+
+# (NOT IMPLEMENTED) apply_to uses proper module names not aliases 
+def patch_module(scope:ModuleType|Callable,apply_to:str,patch:Callable)->Callable[[],None]:
+	
+	
+	#wallrus operator allows for assignment without getting in way of condition
+	if not(scope_module := inspect.getmodule(scope)):
+		raise TypeError("Scope must be a module or function")
+	
+
+	#from this point on we are assuming apply_to is module.function
+
+
+
+	try:
+		apply_module_name,apply_func_name=apply_to.rsplit('.',1)
+	except ValueError:
+		raise ValueError("apply_to must contain moudle part")
+	
+
+	apply_module:ModuleType=getattr(scope_module,apply_module_name)
+	original_func:Callable=getattr(apply_module,apply_func_name)
+
+
+	dummy_module_class:type
+	if inspect.isclass(apply_module):
+		dummy_module_class=type(f"_{apply_module_name}_dummy",(apply_module,),{})
+
+	else:
+		#if its not already loaded load dummy_module_class with real module attributes here
+		dummy_module_class=type(f"_{apply_module_name}_dummy",(),{})
+		copy_module_to_class(apply_module,dummy_module_class)
+
+
+
+	#if we want to limit the scope of our patch to a function we need to wrap 
+	#the patch function in a wrapper that inspects the call stack and 
+	#dispatches to the correct function for that context
+
+	target_caller_name=scope.__name__
+	def _context_switch(*args, **kwargs):
+		if(target_caller_name==_getframe(1).f_code.co_name):
+			return patch(*args,**kwargs)
+		else:
+			return original_func(*args,**kwargs)
+
+	wrapped_patch:Callable=_context_switch if inspect.isfunction(scope) else patch
+
+
+
+
+
+	setattr(dummy_module_class,apply_func_name,wrapped_patch)
+	#replace imported module with our dummy class 
+	setattr(scope_module,apply_module_name,dummy_module_class)
+	
+
+	def revert_patch():
+			delattr(scope_module,apply_module_name)
+			setattr(scope_module,apply_module_name,apply_module)
+
+
+
+	
+
+	return revert_patch
+	
+
+
+
+
+
+
+
+
+
+
+def simulate_input(scope,feed_tape:Iterator[str]|Iterable[str])->Callable[[],None]:
+	_feed_tape=iter(feed_tape)
 	def _patched_input(Prompt=None):
-		if InputAutofeed._feed_tape:
-			return InputAutofeed._feed_tape.popleft()
-		raise IndexError("Unexpectedly ran into end of feed tape.")
-
-		
-	@classmethod
-	def enable_simulated_input(cls,module_to_patch:ModuleType,feed_tape:Iterable[str]):
-		"""
-		Enables simulated input to be used in place of user input
-	
-		:param module_to_patch: module we want to shadow the input function in.
-		:param feed_tape: simulated input to feed when input is requested. 
-		 Input is fed FILO (starts at index 0).
-		:raises IndexError: whenever the module tries to call the input function 
-		more times than feed_tape has simulated inputs
-		"""
-		cls._feed_tape=deque(feed_tape)
-		setattr(module_to_patch,"input",cls._patched_input)
-		
-	
-	@classmethod 
-	def disable_simulated_input(cls):
-		"""unnecisary but still nice to clean up"""
-		if not cls._loaded_module:return
-		delattr(cls._loaded_module,"input")
-		cls._loaded_module=None
+		return next(_feed_tape)
+	return monkey_patch_func(scope,"input",_patched_input)
 
 
-	def __init__(self):
-		raise TypeError(f"{self.__class__.__name__} is not instantiable")
+#provides some limited additional safety 
+#TODO: add ability to limit scope to function
+#TODO: add functionality to limit scope to classes
+def monkey_patch_func(scope_to_patch:ModuleType|Callable,apply_to:str,patch_function:Callable)->Callable[[],None]:
 	
+
+	
+	#check if we are shadowing an existing function
+	try:
+		old_function=getattr(scope_to_patch,apply_to)
+		def revert_patch():
+			delattr(scope_to_patch,apply_to)
+			setattr(scope_to_patch,apply_to,old_function)
+				
+	except AttributeError:
+		def revert_patch():
+			delattr(scope_to_patch,apply_to)
+
+	setattr(scope_to_patch,apply_to,patch_function)
+
+	return revert_patch
