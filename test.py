1a0d6c1064b28b7f017b7a409c13ba9ddd8890d7
afan21 <afan2211@gmail.com>
2026-04-09 15:11:46 -0500

    merged local wip version

--- test.py
+++ test.py
@@ -4,14 +4,102 @@
 # summary: automated tests for SG2_Program
 
 import testing_utilities
-import SG2_Alex
+import SG2_Program
 
+import matplotlib as mp
+
+def over(*args):
+	for arg in args:
+		print(arg)
+
+import matplotlib.pyplot as plot
+
+import importlib.util
+from types import ModuleType
+from typing import Callable,List,Type
+import inspect
+
+
+
+import pathlib 
+
+from collections.abc import Buffer as ReadableBuffer
+
+
+
+def test1():
+	revert_input_patch=testing_utilities.simulate_input(SG2_Program,["a","b"])
+	SG2_Program.main()
+	revert_input_patch()
+
+def test2():
+	reel=[["a","b"],["b","c"],["c","d"]]
+	for tape in reel:
+		revert_input_patch=testing_utilities.simulate_input(SG2_Program,["a","b"])
+		SG2_Program.main()
+		revert_input_patch()
+
+
+def test3():
+	
+	revert=testing_utilities.patch_module(SG2_Program.main,"plt.subplots")
+	SG2_Program.main()
+	revert()
+
+
+
+
+
+
+class dummy_file():
+	def __enter__(self):
+		return self
+	
+	def __exit__(self,exc_type, exc_value, traceback):
+		pass
+
+	def write(self,s: ReadableBuffer,/) -> int:
+		print(s)
+		return 0
+
+
+def dummy_open(*args,**kwargs): 
+	return dummy_file()
+
+
+
+
+
+def test4():
+	""" Test patching instances via hijacking a factory method. Patches a file instances write method."""
+	#possible interface? scope,instance constructor,instance_method_to_patch,patch
+	
+	revert_intercept=testing_utilities.patch_module(SG2_Program.write_results,"Path.open",dummy_open)
+	
+	try:
+		SG2_Program.main()
+	finally:                                                             
+		revert_intercept()
+		
+
+
+#basic sim test
+def test5():
+	
+	print(SG2_Program.run_simulations(50,1000,300))
 
 
 def main():
-	testing_utilities.InputAutofeed.enable_simulated_input(SG2_Alex, ["a", "b"])
-	SG2_Alex.main()
-	testing_utilities.InputAutofeed.disable_simulated_input()
+	print("dummy")
+
+	#testing_utilities.monkey_patch_func(SG2_Program.plt,"subplots",wrapper)
+	#testing_utilities.monkey_patch_func(mp.axes.Axes,"hist",over)
+	#testing_utilities.monkey_patch_func(SG2_Program.main,"ax",new_ax)
+
+	#load(plot,plt)
+	#print(plt.subplots)
+	#setattr(SG2_Program.main,"plt",plt)
+	
 
 
 
